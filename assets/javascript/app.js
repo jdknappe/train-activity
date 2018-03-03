362c15fe340b9165fb4a02e0bacd0bb8751c0c56
Jeremy Knappe <jdknappe@me.com>
2018-03-02 21:35:56 -0800

    first files for train activity

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAw-M8-vQsqAAJaETt5fRTCUPu32gPl4aM",
+        authDomain: "train-activity-6aaab.firebaseapp.com",
+        databaseURL: "https://train-activity-6aaab.firebaseio.com",
+        projectId: "train-activity-6aaab",
+        storageBucket: "train-activity-6aaab.appspot.com",
+        messagingSenderId: "306393814865"
+    };
+    firebase.initializeApp(config);
+
+    
+})
