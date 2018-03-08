d80055c92d5003b2bab5823267443d94649a0c35
Jeremy Knappe <jdknappe@me.com>
2018-03-07 18:32:36 -0800

    another commit for submission

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,14 @@
-$(document).ready(function() {
+// all code should go within the document ready function
+$(document).ready(function () {
+
+    var trainName = $('#train-name').val().trim();
+    // * Destination 
+    var destination = $('#destination').val().trim();
+    // * First Train Time -- in military time
+    var firstTrainMilitary = $('#first-train-military-time').val().trim();
+    // * Frequency -- in minutes
+    var frequency = $('#frequency').val().trim();
+
     // Initialize Firebase
     var config = {
         apiKey: "AIzaSyAw-M8-vQsqAAJaETt5fRTCUPu32gPl4aM",
@@ -10,5 +20,52 @@ $(document).ready(function() {
     };
     firebase.initializeApp(config);
 
-    
-})
+    var database = firebase.database()
+
+    function getHTMLTrain() {
+        var row = $("<div/>");
+        row.addClass("row");
+        var tableData = $("<td>").html("<tr>" + trainName + destination + firstTrainMilitary + frequency + "</tr>")
+        row.append(table)
+        return row;
+    }
+
+    function addTrainToFirebase() {
+        database.ref().push({
+            trainName: trainName,
+            destination: destination,
+            firstTrainMilitary: firstTrainMilitary,
+            frequency: frequency
+        }) 
+    }
+
+    database.ref().on("child_added", function(childSnapshot) {
+        console.log(childSnapshot.val())
+    })
+
+    function getNextTrainTime(currentTime, firstTrainMilitary, frequency) {
+        var nextTrainTime = "get moment object and put here";
+        // Use moment.js to calculate nextTrainTime based on currentTime, firstTrainMilitary, and frequency.
+        return nextTrainTime;
+
+
+    }
+
+    $('form').on('submit', function () {
+        prevent
+        // * Train Name
+        var trainName = $('#train-name').val().trim();
+        // * Destination 
+        var destination = $('#destination').val().trim();
+        // * First Train Time -- in military time
+        var firstTrainMilitary = $('#first-train-military-time').val().trim();
+        // * Frequency -- in minutes
+        var frequency = $('#frequency').val().trim();
+
+        // Will use firebase to add this train to our database.
+    });
+
+
+
+    //End document.ready... no code below this line.
+});
