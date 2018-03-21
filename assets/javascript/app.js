3f009760b911ed0830ec342334e3ddf6a6309c55
Jeremy Knappe <jdknappe@me.com>
2018-03-21 11:22:44 -0700

    updated working train app

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,71 +1,55 @@
-// all code should go within the document ready function
-$(document).ready(function () {
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAw-M8-vQsqAAJaETt5fRTCUPu32gPl4aM",
+    authDomain: "train-activity-6aaab.firebaseapp.com",
+    databaseURL: "https://train-activity-6aaab.firebaseio.com",
+    projectId: "train-activity-6aaab",
+    storageBucket: "train-activity-6aaab.appspot.com",
+    messagingSenderId: "306393814865"
+};
+firebase.initializeApp(config);
 
+var trainData = firebase.database()
+
+$('#submitBTN').on("click", function(){
     var trainName = $('#train-name').val().trim();
-    // * Destination 
     var destination = $('#destination').val().trim();
-    // * First Train Time -- in military time
-    var firstTrainMilitary = $('#first-train-military-time').val().trim();
-    // * Frequency -- in minutes
+    var firstTrain = moment($('#first-train-military-time').val().trim(), "HH:mm").subtract(10, "years").format("X");
     var frequency = $('#frequency').val().trim();
+    console.log(firstTrain);
 
-    // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyAw-M8-vQsqAAJaETt5fRTCUPu32gPl4aM",
-        authDomain: "train-activity-6aaab.firebaseapp.com",
-        databaseURL: "https://train-activity-6aaab.firebaseio.com",
-        projectId: "train-activity-6aaab",
-        storageBucket: "train-activity-6aaab.appspot.com",
-        messagingSenderId: "306393814865"
-    };
-    firebase.initializeApp(config);
-
-    var database = firebase.database()
-
-    function getHTMLTrain() {
-        var row = $("<div/>");
-        row.addClass("row");
-        var tableData = $("<td>").html("<tr>" + trainName + destination + firstTrainMilitary + frequency + "</tr>")
-        row.append(table)
-        return row;
+    var newTrain = {
+        name: trainName,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency
     }
 
-    function addTrainToFirebase() {
-        database.ref().push({
-            trainName: trainName,
-            destination: destination,
-            firstTrainMilitary: firstTrainMilitary,
-            frequency: frequency
-        }) 
-    }
+    trainData.ref().push(newTrain)
 
-    database.ref().on("child_added", function(childSnapshot) {
-        console.log(childSnapshot.val())
-    })
+    alert("Train Added!");
 
-    function getNextTrainTime(currentTime, firstTrainMilitary, frequency) {
-        var nextTrainTime = "get moment object and put here";
-        // Use moment.js to calculate nextTrainTime based on currentTime, firstTrainMilitary, and frequency.
-        return nextTrainTime;
+    $('#trainName').val('')
+    $('#destination').val('')
+    $('#firstTrain').val('')
+    $('#frequency').val('')
 
+    return false;
+})
 
-    }
+trainData.ref().on('child_added', function(snapshot){
+    var name = snapshot.val().name
+    var destination = snapshot.val().destination
+    var frequency = snapshot.val().frequency
+    var firstTrain = snapshot.val().firstTrain
 
-    $('form').on('submit', function () {
-        prevent
-        // * Train Name
-        var trainName = $('#train-name').val().trim();
-        // * Destination 
-        var destination = $('#destination').val().trim();
-        // * First Train Time -- in military time
-        var firstTrainMilitary = $('#first-train-military-time').val().trim();
-        // * Frequency -- in minutes
-        var frequency = $('#frequency').val().trim();
+    var remainder = moment().diff(moment.unix(firstTrain), 'minutes')%frequency
+    var minutes = frequency - remainder
+    var arrival = moment().add(minutes, 'm').format("hh:mm A")
 
-        // Will use firebase to add this train to our database.
-    });
+    console.log(remainder)
+    console.log(minutes)
+    console.log(arrival)
 
-
-
-    //End document.ready... no code below this line.
-});
+    $('#trainTable').append('<tr><td>' + name + '</td><td>' + destination + '</td><td>' + frequency + '</td><td>' + arrival + '</td><td>' + minutes + '</td><tr>');
+})
